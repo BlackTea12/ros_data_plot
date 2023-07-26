5fac0d7c5f2ae0af92617b2a81a4f317a1459a1c
BlackTea12 <BlackTea12@github.com>
2023-07-26 15:33:36 +0900

    [add] data tip with average value added

--- src/one_float.py
+++ src/one_float.py
@@ -9,6 +9,7 @@ from std_msgs.msg import Float64
 bag_path = rospy.get_param("rosbag_path_dir")
 bag_name = rospy.get_param("rosbag_name")
 img_path = rospy.get_param("img_path_dir")
+
 def callback(data, t):
     if data._type == 'std_msgs/Float64':
         # Append the received Float64 data to a list
@@ -16,6 +17,10 @@ def callback(data, t):
         exec_time_time_list.append(t)
 
 if __name__ == '__main__':
+    # saving total data
+    total_val1 = 0
+    total_val2 = 0
+
     # Initialize the ROS node
     rospy.init_node('plot_logger', anonymous=True)
 
@@ -50,11 +55,42 @@ if __name__ == '__main__':
     # Create subplots
     # Plot the Float64 data
     plt.plot(exec_time_time_list, exec_time_list, color='#9467bd')
-    # plt.ylim([0, 50])
+    plt.ylim([0, 55])
+    plt.xlim([7, 47])
     plt.xlabel('travel duration[sec]')
     plt.ylabel('execution time [ms]')
     plt.grid(axis='y')
 
+    # Plot avg value
+    time1 = []
+    time2 = []
+    for i in range(len(exec_time_time_list)):
+        if exec_time_time_list[i] < 19 and exec_time_time_list[i] > 7:
+            total_val1 += exec_time_list[i]
+            time1.append(exec_time_time_list[i])
+        elif exec_time_time_list[i] > 35 and exec_time_time_list[i] < 47:
+            total_val2 += exec_time_list[i]
+            time2.append(exec_time_time_list[i])
+
+    total_val1 /= len(time1)
+    total_val2 /= len(time2)
+    y_avg1 = [total_val1 for i in range(len(time1))]
+    y_avg2 = [total_val2 for i in range(len(time2))]
+    plt.plot(time1, y_avg1, '--', color='#7CFC00')
+    plt.plot(time2, y_avg2, '--', color='#7CFC00')
+
+    # tip to annotate
+    index_an1 = round(len(y_avg1)/2)
+    index_an2 = round(len(y_avg2)/2)
+    plt.annotate(f'avg: ({round(y_avg1[index_an1],2)})',
+             xy=(time1[index_an1], y_avg1[index_an1]),
+             xytext=(time1[index_an1] + 0.5, y_avg1[index_an1] - 3),
+             arrowprops=dict(facecolor='black', shrink=0.05))
+    plt.annotate(f'avg: ({round(y_avg2[index_an2],2)})',
+             xy=(time2[index_an2], y_avg2[index_an2]),
+             xytext=(time2[index_an2] + 0.5, y_avg2[index_an2] - 3),
+             arrowprops=dict(facecolor='black', shrink=0.05))
+    
     # Adjust the layout of the subplots
     plt.legend(loc='best')
     plt.tight_layout()
